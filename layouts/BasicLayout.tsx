d68a27940a1ed6779838fb2ebc2ba63f699c77c8
greg <gsc229@gmail.com>
2021-07-09 14:28:36 -0400

    m-ui css layout and Container in BasicLayout

--- layouts/BasicLayout.tsx
+++ layouts/BasicLayout.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import Footer from '../components/Footer/Footer'
 import Header from '../components/Header/Header'
+import Container from '@material-ui/core/Container'
 
 
 const BasicLayout = ({ children }:{ children:React.ReactNode }) => {
   return (
-    <div className='basic-layout'>
+    <Container className='basic-layout'>
       <Header />
         {children}
       <Footer />
-    </div>
+    </Container>
   )
 }
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { SchoolsProvider } from '../src/store'
+import { CssBaseline } from '@material-ui/core'
 
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <SchoolsProvider>
-      <Component {...pageProps} />
+      <CssBaseline>
+        <Component {...pageProps} />
+      </CssBaseline>
     </SchoolsProvider>
   )
 }
